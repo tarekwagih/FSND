bf87787bd9c53be41d38d521df4c77f06c50ff20
tarekwagih43 <tarekwagih43@gmail.com>
2021-01-08 23:23:43 +0200

    models file

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -18,7 +18,7 @@ from pprint import pprint
 from flask_migrate import Migrate
 from sqlalchemy.orm import aliased
 
-# from models import db_create_all
+from models import db_create_all
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -30,63 +30,7 @@ db = SQLAlchemy(app, session_options={"expire_on_commit": False})
 # TODO: connect to a local postgresql database (Connected To Database # Tarek Wagih)
 migrate = Migrate(app, db)
 
-# db_create_all(db, babel)
-
-class Venue(db.Model):
-    __tablename__ = 'venues'
-    __table_args__ = {'extend_existing': True}
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=True)
-    state = db.Column(db.String(120), nullable=True)
-    address = db.Column(db.String(120), nullable=True)
-    phone = db.Column(db.String(120), nullable=True)
-    genres = db.Column(db.String(), nullable=True)
-    image_link = db.Column(db.String(500), nullable=True)
-    facebook_link = db.Column(db.String(120), nullable=True)
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    website = db.Column(db.String(120), nullable=True)
-    seeking_talent = db.Column(db.Integer, nullable=True)
-    seeking_description = db.Column(db.String(500), nullable=True)
-
-    # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-    show_id = db.relationship('Show', backref='Venue', cascade="all, delete-orphan", lazy=True)
-
-class Artist(db.Model):
-    __tablename__ = 'artists'
-    __table_args__ = {'extend_existing': True}
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=True)
-    state = db.Column(db.String(120), nullable=True)
-    phone = db.Column(db.String(120), nullable=True)
-    genres = db.Column(db.String(), nullable=True)
-    image_link = db.Column(db.String(500), nullable=True)
-    facebook_link = db.Column(db.String(120), nullable=True)
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    website = db.Column(db.String(120), nullable=True)
-    seeking_venue = db.Column(db.Integer, nullable=True)
-    seeking_description = db.Column(db.String(500), nullable=True)
-
-    # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-    show_id = db.relationship('Show', backref='Artist', cascade="all, delete-orphan", lazy=True)
-
-
-# TODO Implement Show Model
-
-class Show(db.Model):
-    __tablename__ = 'shows'
-    __table_args__ = {'extend_existing': True}
-
-    id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
-    date = db.Column(db.DateTime, default=babel.dates.format_datetime(), nullable=False)
-
+db_create_all(db, babel)
 
 
 #----------------------------------------------------------------------------#
