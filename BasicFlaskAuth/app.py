849e056c4b248314571b90520fa84950c953f8ee
tarekwagih43 <tarekwagih43@gmail.com>
2021-01-25 16:19:26 +0200

    requires

--- BasicFlaskAuth/app.py
+++ BasicFlaskAuth/app.py
@@ -4,13 +4,11 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
+AUTH0_DOMAIN = 'basic-tarek.us.auth0.com/'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
-
+API_AUDIENCE = 'training'
 
 class AuthError(Exception):
     def __init__(self, error, status_code):
